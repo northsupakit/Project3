bc243ee620889d639b698f3a2200b7ac5a9e0c86
Supakit Wiriyachinnakarn <northw@login35.chn.perlmutter.nersc.gov>
2024-04-28 15:32:07 -0700

    ran the files

---
+++ build/CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o: \
+ /global/homes/n/northw/Project3/dgemv-basic.cpp \
+ /usr/include/stdc-predef.h
---
+++ build/CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o: \
+ /global/homes/n/northw/Project3/dgemv-vectorized.cpp \
+ /usr/include/stdc-predef.h
